3d598c7c7e12f9ddee1318a9ac228b2c9a898ee5
Ayazzsaifi <104493436+Ayazzsaifi@users.noreply.github.com>
2026-09-07 22:40:00 +0530

    chore: add app entry point, assets and config

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RootTabs from './src/navigation/RootTabs';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <RootTabs />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+}
